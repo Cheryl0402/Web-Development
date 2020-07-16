044adf3e2c2c5418c48c54c63d9a32cb3ac03f09
cheryl0402 <yi.cheryl.chen@gmail.com>
2020-07-15 17:29:16 -0700

    jquery.js completed'

---
+++ Practice/jqueryEffect.js
@@ -0,0 +1,16 @@
+$("button").on("click",function(){
+	// $("div").fadeOut(1000, function(){
+	// 	// put this inside will give you effect print after fade out
+	// 	console.log("Fade out completed");
+	// 	// $(this).remove();
+	// });
+	// console.log("Fade out completed"); here will give you this line directly
+  //   $("p").fadeIn(2000,function(){
+		// console.log("Fade in completed");
+  //   })
+
+	// $('div').fadeToggle(1000);
+	$("div").slideDown(1000);
+});
+
+
