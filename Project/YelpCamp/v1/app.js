c5bf105e8dbfb22449e49edeb8dd5cf7111123fa
cheryl0402 <yi.cheryl.chen@gmail.com>
2020-08-15 21:46:31 -0700

    Set up new campground POST route, add in body-parser, set up route to show form, add basic unstyled form

--- Project/YelpCamp/v1/app.js
+++ Project/YelpCamp/v1/app.js
@@ -1,21 +1,49 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+
 
 app.get("/", function(req,res){
 	res.render("landing");
 });
 
-app.get("/campgrounds", function(req,res){
+
 	var campgrounds = [
 		{name:"Salmon Creek", image:"https://www.photosforclass.com/download/px_2419278"},
 		{name:"Granite Hill", image:"https://www.photosforclass.com/download/px_803226"},
 		{name:"Mountain Goat's Rest", image:"https://www.photosforclass.com/download/pb_1851092"}
 	]
 
+app.get("/campgrounds", function(req,res){
+
 	res.render("campgrounds", {campgrounds:campgrounds});
 });
+
+
+//send a new post to create new campground
+app.post("/campgrounds", function(req, res){
+	//get data from form and add to campground array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name:name, image: image}
+	campgrounds.push(newCampground);
+
+	//redirect back to campground page
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res){
+	res.render("new");
+})
+
+
+
+
+
 
 app.listen(3000, function(){
 	console.log("The YelpCamp Server is listening at port 3000");
